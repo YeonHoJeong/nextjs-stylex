6f5920ec61a217a9ed92a839efc544136a377d0a
yeonhojeong <dusgh2101@naver.com>
2024-04-24 01:35:11 +0900

    Feat : styleX test button 추가

---
+++ src/app/components/Button/index.tsx
@@ -0,0 +1,18 @@
+import type { StyleXStyles } from "@stylexjs/stylex";
+import { PropsWithChildren } from "react";
+import * as stylex from "@stylexjs/stylex";
+
+const styles = stylex.create({
+  buttonColor: {
+    backgroundColor: "#ffffff",
+    color: "#fe32dd",
+  },
+});
+
+type ButtonProps = {};
+
+function Button({ children }: PropsWithChildren<ButtonProps>) {
+  return <button {...stylex.props(styles.buttonColor)}>{children}</button>;
+}
+
+export default Button;
